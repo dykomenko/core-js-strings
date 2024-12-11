e8f1291a24aab77472b498140559905c897dd5c9
Kirill <kirill.dykomenko@gmail.com>
2024-12-11 17:32:03 +0300

    feat: 8 tasks done

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -38,7 +38,19 @@ function getStringLength(value) {
  *   isString(new String('test')) => true
  */
 function isString(value) {
-  return typeof value === 'string' && value;
+  let out = false;
+  if (
+    value === undefined ||
+    value === null ||
+    arguments.length === 0 ||
+    value === false ||
+    typeof value.valueOf() !== 'string'
+  ) {
+    out = false;
+  } else {
+    out = true;
+  }
+  return out;
 }
 
 /**
@@ -53,8 +65,8 @@ function isString(value) {
  *   concatenateStrings('aa', '') => 'aa'.
  *   concatenateStrings('', 'bb') => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings(value1, value2) {
+  return value1.concat('', value2);
 }
 
 /**
@@ -68,8 +80,8 @@ function concatenateStrings(/* value1, value2 */) {
  *   getFirstChar('cat') => 'c'
  *   getFirstChar('') => ''
  */
-function getFirstChar(/* value */) {
-  throw new Error('Not implemented');
+function getFirstChar(value) {
+  return value.charAt(0);
 }
 
 /**
@@ -83,8 +95,8 @@ function getFirstChar(/* value */) {
  *   removeLeadingAndTrailingWhitespaces('cat ') => 'cat'
  *   removeLeadingAndTrailingWhitespaces('\t\t\tHello, World! ') => 'Hello, World!'
  */
-function removeLeadingAndTrailingWhitespaces(/* value */) {
-  throw new Error('Not implemented');
+function removeLeadingAndTrailingWhitespaces(value) {
+  return value.trim();
 }
 
 /**
